6388915249eea5e6b4eaa27721e3090bb7d1807e
shijyuan <38323700+shijyuen@users.noreply.github.com>
2020-06-30 13:59:55 +0800

    change theme

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import guzzle_sphinx_theme
 extensions = [
 ]
 
@@ -78,7 +79,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+extensions.append("guzzle_sphinx_theme")
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
