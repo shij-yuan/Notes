599e17c47d1a16664bbfec239adf058e059eb08b
shijyuan <38323700+shijyuen@users.noreply.github.com>
2020-07-22 17:06:35 +0800

    test tables

--- source/conf.py
+++ source/conf.py
@@ -54,10 +54,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+
 
 source_suffix = ['.rst', '.md']
 
